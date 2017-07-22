9ffd61727258bcb5495fc58879659b91a57175cf
Sergio Mendoza <smendoza787@gmail.com>
2017-07-21 22:01:29 -0700

    added hide hyykers feeature

---
+++ app/assets/javascripts/trails.js
@@ -0,0 +1,8 @@
+function Trail(id, name, location, distance, elevation, users) {
+  this.id = id;
+  this.name = name;
+  this.location = location;
+  this.distance = distance;
+  this.elevation = elevation;
+  this.users = users;
+}
