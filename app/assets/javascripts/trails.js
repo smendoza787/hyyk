18716f87826025b544a3a2ab707dd365e6d7741e
Sergio Mendoza <smendoza787@gmail.com>
2017-07-22 01:29:28 -0700

    cant figure out why show page AJAX is not working, only renders the next resource but nothing after that...

--- app/assets/javascripts/trails.js
+++ app/assets/javascripts/trails.js
@@ -1,8 +1,9 @@
-function Trail(id, name, location, distance, elevation, users) {
+function Trail(id, name, location, distance, elevation, trail_type, users) {
   this.id = id;
   this.name = name;
   this.location = location;
   this.distance = distance;
   this.elevation = elevation;
+  this.trail_type = trail_type;
   this.users = users;
 }
