afa7d4ad56402aa912f2285b5ac772588618c1f7
Sergio Mendoza <smendoza787@gmail.com>
2017-07-24 02:33:25 -0700

    changed comment delete from button to link, changed the size of dummy user avatars

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,3 +1,4 @@
 function Comment(content, user_id, trail_id) {
   this.content = content;
+  this.userId = user_id;
 }
--- app/assets/javascripts/trails.js
+++ app/assets/javascripts/trails.js
@@ -1,4 +1,4 @@
-function Trail(id, name, location, distance, elevation, trail_type, users) {
+function Trail(id, name, location, distance, elevation, trail_type, users, comments) {
   this.id = id;
   this.name = name;
   this.location = location;
@@ -6,4 +6,5 @@ function Trail(id, name, location, distance, elevation, trail_type, users) {
   this.elevation = elevation;
   this.trail_type = trail_type;
   this.users = users;
+  this.comments = comments;
 }
